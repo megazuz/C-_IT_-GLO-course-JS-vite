ef910b2a83d129ae256ae4c866beb3a068a32078
unknown <megazuz@gmail.com>
2024-07-09 01:33:03 +0300

    lesson 19 finished

--- lesson19/exercise1.js
+++ lesson19/exercise1.js
@@ -3,13 +3,9 @@ const page = document.body;
 const dayPeriodsRU = ['Доброй ночи', 'Доброе утро', 'Добрый день', 'Добрый вечер'];
 const weekDayRU = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 const timeNow = new Date();
-
 const nextNY = new Date('1 january ' + (timeNow.getFullYear() + 1));
 
 page.innerHTML = `<p>${dayPeriodsRU[Math.floor(timeNow.getHours() / 6)]}<p>
-
 <p>Сегодня: ${weekDayRU[timeNow.getDay()]}</p>
-
 <p>Текущее время: ${timeNow.toTimeString().substring(0, 8)}</p>
-
 <p>До нового года осталось ${Math.floor((nextNY - timeNow) / 86400000)} дней</p>`;
