2dfdc26fa35d305f8e55d72b89b5ccb30747079e
ZUZ <megazuz@gmail.com>
2024-07-08 23:46:45 +0300

    середина первого задания 19 урока

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -3,24 +3,26 @@ export const timer = (deadline) => {
   const timerHours = document.getElementById('timer-hours');
   const timerMinutes = document.getElementById('timer-minutes');
   const timerSeconds = document.getElementById('timer-seconds');
+  let dateStop = new Date(deadline).getTime();
+  let dateNow = new Date().getTime();
 
   const getTimeRemaining = () => {
-    let dateStop = new Date(deadline).getTime();
-    let dateNow = new Date().getTime();
+    dateNow = new Date().getTime();
     let timeRemaining = (dateStop - dateNow) / 1000;
     let hours = Math.floor(timeRemaining / 3600);
-    let minutes = Math.floor((timeRemaining % 60) / 60);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
     let seconds = Math.floor(timeRemaining % 60);
     return { timeRemaining, hours, minutes, seconds };
   };
   const updateClock = () => {
     let getTime = getTimeRemaining();
-    timerHours.textContent = getTime.hours;
-    timerMinutes.textContent = getTime.minutes;
-    timerSeconds.textContent = getTime.seconds;
+    timerHours.textContent = getTime.hours < 10 ? `0${getTime.hours}` : getTime.hours;
+    timerMinutes.textContent = getTime.minutes < 10 ? `0${getTime.minutes}` : getTime.minutes;
+    timerSeconds.textContent = getTime.seconds < 10 ? `0${getTime.seconds}` : getTime.seconds;
     if (getTime.timeRemaining > 0) {
       setInterval(updateClock, 1000);
     }
   };
-  setInterval(updateClock, 1000);
+
+  dateNow < dateStop ? updateClock() : {};
 };
